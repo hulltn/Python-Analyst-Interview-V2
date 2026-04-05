ffb6a437a006cb9e248ebf57fb25667bebde06fe
hulltn <70494184+hulltn@users.noreply.github.com>
2026-04-05 17:12:02 -0400

    Correct syntax and structure in app.py
    Fix syntax errors in Flask app routes and add imports.

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+#imports Flask, allows referencing of Flask API. Imports random table for randomized variables.
+from flask import Flask, render_template, jsonify
+import random
 
+app = Flask(__name__)
+
+#defines symbols for game
+symbols = ["Atlanta","Cincinnati","Detroit","Dallas","Denver"]
+
+#variable definition 
+@app.route("/")
+def index();
+  return render_template("index.html")
+
+@app.route("/spin")
+def spin()
+  result = [random.choice(symbols) for _ range(3)
+  return jsonify(result)
+
+if __name__ =="__main__"
+    app.run(host="0.0.0.0", port=3000)
